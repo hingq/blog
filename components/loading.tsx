896b3c848ed75ee619f23c75364f2f77793e653b
heq <amazyko@foxmail.com>
2026-05-25 21:49:30 +0800

    feat:add

--- components/loading.tsx
+++ components/loading.tsx
@@ -1,8 +1,8 @@
 export default function Loading() {
   return (
-    <div className="fixed top-0 left-0 right-0 z-80">
-      <div className="h-0.5 w-full overflow-hidden bg-primary-200 dark:bg-primary-900">
-        <div className="h-full w-full origin-left animate-progress-bar bg-primary-600 dark:bg-primary-400" />
+    <div className="fixed top-0 right-0 left-0 z-80">
+      <div className="bg-primary-200 dark:bg-primary-900 h-0.5 w-full overflow-hidden">
+        <div className="animate-progress-bar bg-primary-600 dark:bg-primary-400 h-full w-full origin-left" />
       </div>
     </div>
   )
